f2207bbb04c549ccaaa7f679d0c4ece2553fb20b
Joe Bhullar <joebhullar11@gmail.com>
2022-02-14 16:14:32 -0700

    WebAngularProject - ComponentCreation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './Component/help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule
