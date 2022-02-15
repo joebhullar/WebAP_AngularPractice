956db487fbfc26a93a93f73ccaa5353bd1820e0b
Joe Bhullar <joebhullar11@gmail.com>
2022-02-15 00:25:31 -0700

    Feedback Service Implemented Need to do product service

--- src/app/Component/create-feedback/create-feedback.component.ts
+++ src/app/Component/create-feedback/create-feedback.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Feedback } from 'src/app/Model/Feedback';
+import { FeedbackService } from 'src/app/Services/feedback.service';
 
 @Component({
   selector: 'app-create-feedback',
   templateUrl: './create-feedback.component.html',
-  styleUrls: ['./create-feedback.component.css']
+  styleUrls: ['./create-feedback.component.css'],
+  providers:[FeedbackService]
 })
 export class CreateFeedbackComponent implements OnInit {
-
-  constructor() { }
+  createFeedback:Feedback[];
+  constructor(private _feedbackservice:FeedbackService, private _router:Router) { }
 
   ngOnInit(): void {
   }
+  SaveFeedback(temp: any) {
+    this._feedbackservice.SaveFeedback(temp).subscribe( Successfully => {
+          console.log('Feedback Created Successfully');
+          alert('Feedback Successfully');
+         },
+    Error => {
+       console.log('Something Error');
+       alert('Something Error');
+    });
+ }
 
+ ViewFeedback(){
+  this._router.navigate(['Feedback'])
+}
 }
--- src/app/Services/feedback.service.ts
+++ src/app/Services/feedback.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Feedback } from '../Model/Feedback';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable()
 export class FeedbackService {
+  private headers: HttpHeaders;
+  private url:string = 'http://localhost:58052/api/Feedback/FeedbackList';
+  private createFeedbackurl:string = 'http://localhost:58052/api/Feedback/CreateFeedback';
+  constructor(private http:HttpClient) {   }
+  public getFeedback():Observable<Feedback[]>
+  {
+    // return this.http.get<Feedback[]>(this.url).pipe(catchError(this.handleError));
+    return this.http.get<Feedback[]>(this.url)  ;
+  }
+  handleError(error){
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      errorMessage = 'Error: ${error.error.message}';
+    }else{
+      errorMessage=`Error Code:${error.status}\nMessage:  ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
-  constructor() { }
+  
+  SaveFeedback(temp): Observable<Feedback[]> {
+    alert('hi');
+    alert(temp.value);
+    console.log(temp.value);
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    // tslint:disable-next-line:max-line-length
+    alert(temp.value.feedbackGiven);
+    alert(temp.value.feedbackBy);
+    const body = {feedbackGiven: temp.value.feedbackGiven,feedbackBy: temp.value.feedbackBy,feedbackDate: temp.value.feedbackDate };
+    return this.http.post<Feedback[]>(this.createFeedbackurl, body, {headers});
+}
+
 }
